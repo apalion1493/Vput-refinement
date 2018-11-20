2725c074516de984323a1e20e6c93b2d48e393be
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2018-11-20 16:35:10 +0200

    вставил анимацию

--- assets/js/common.js
+++ assets/js/common.js
@@ -114,3 +114,61 @@ jQuery(document).ready(function($) {
 
     AOS.init();
 });
+
+
+window.onload = function(){
+
+    var playBtn = document.getElementById("playBtn"),
+        pauseBtn = document.getElementById("pauseBtn"),
+        restartBtn = document.getElementById("restartBtn");
+
+    TweenMax.to("#vput", 1, {opacity: 1, delay: 1});
+    TweenMax.to("#vput-label", 1, {opacity: 1, delay: 1});
+    TweenMax.to("#line_4-left", 1, {opacity: 1, delay: 1.5});
+    TweenMax.to("#partner", 1, {opacity: 1, delay: 2});
+    TweenMax.to("#partner-text", 1, {opacity: 1, delay: 2});
+    TweenMax.to("#Line2-top", 1, {opacity: 1, delay: 2.5});
+    TweenMax.to("#parter-links", 1, {opacity: 1, delay: 2.5});
+    TweenMax.to("#websites", 1, {opacity: 1, delay: 3});
+    TweenMax.to("#Oval7", 1, {opacity: 1, delay: 3});
+    TweenMax.to("#Line2-center", 1, {opacity: 1, delay: 3.5});
+    TweenMax.to("#blogs", 1, {opacity: 1, delay: 4});
+    TweenMax.to("#Oval7Copy6", 1, {opacity: 1, delay: 4});
+    TweenMax.to("#Line2-bottom", 1, {opacity: 1, delay: 4.5});
+    TweenMax.to("#socials", 1, {opacity: 1, delay: 5});
+    TweenMax.to("#Oval7Copy", 1, {opacity: 1, delay: 5});
+    TweenMax.to("#Line3", 1, {opacity: 1, delay: 6});
+    TweenMax.to("#Line2Copy", 1, {opacity: 1, delay: 6.5});
+    TweenMax.to("#referal", 1, {opacity: 1, delay: 7});
+    TweenMax.to("#referals", 1, {opacity: 1, delay: 7});
+    TweenMax.to("#line_4-right", 1, {opacity: 1, delay: 7.5});
+    TweenMax.to("#house-container", 0.5, {opacity: 1, delay: 8, x: "283px"});
+    TweenMax.to("#house-container", 0.5, {opacity: 1, delay: 8.5, y: "-145px"});
+    TweenMax.to("#house-container", 0.5, {opacity: 0, delay: 9, y: "-145px"});
+    TweenMax.to("#Line3Copy-left", 1, {opacity: 1, delay: 9});
+    TweenMax.to("#Line3Copy-right", 1, {opacity: 1, delay: 9});
+    TweenMax.fromTo("#euro", 0.5, {opacity: 0, delay: 10, x: "420px", y: "250px"}, {opacity: 1, delay: 10, x: "425px", y: "135px"});
+    TweenMax.to("#euro", 0.5, {opacity: 1, delay: 10.5, x: "80px", y: "135px"});
+    TweenMax.to("#euro", 0.5, {opacity: 0, delay: 11, x: "80px", y: "105px"});
+    TweenMax.fromTo("#euro", 0.5, {opacity: 0, delay: 11, x: "80px", y: "105px"}, {opacity: 0, delay: 11.1, x: "0px", y: "0px"});
+    TweenMax.to("#euro", 0.5, {opacity: 1, delay: 11.5});
+    TweenMax.fromTo("#percent", 0.5, {opacity: 0, delay: 10, x: "-420px", y: "250px"}, {opacity: 1, delay: 10, x: "-425px", y: "135px"});
+    TweenMax.to("#percent", 0.5, {opacity: 1, delay: 10.5, x: "-80px", y: "135px"});
+    TweenMax.to("#percent", 0.5, {opacity: 0, delay: 11, x: "-80px", y: "105px"});
+    TweenMax.fromTo("#percent", 0.5, {opacity: 0, delay: 11, x: "-80px", y: "105px"}, {opacity: 0, delay: 11.1, x: "0px", y: "0px"});
+    TweenMax.to("#percent", 0.5, {opacity: 1, delay: 11.5});
+
+    playBtn.onclick = function() {
+        tween.play();
+    };
+
+    pauseBtn.onclick = function() {
+        tween.pause();
+    };
+
+    restartBtn.onclick = function() {
+        tween.restart();
+    };
+};
+
+
